a6bfbf20dd1dad3bf5165c685cbfadd922f74613
Ovkorz <pupkaszymon@gmail.com>
2024-05-23 21:34:56 +0200

    delete message api call

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,10 @@
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
 
-
-export default {
-  data() {
-    return {
-      message: ""
-    };
-  }
-}
-
 </script>
 
 <template>
   <main>
     <HelloWorld title="Hello Lingaro!" msg="I deployed this Vue.js app using Azure Static Web App resource." />
   </main>
-  {{  message }}
 </template>
